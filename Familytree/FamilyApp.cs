d24c4b595175b2e4fb088faa1cc8e0e0bdf83bed
borgar90 <73884383+borgar90@users.noreply.github.com>
2021-05-27 12:53:49 +0200

    with FindChildrenOfMother

--- Familytree/FamilyApp.cs
+++ Familytree/FamilyApp.cs
@@ -6,12 +6,13 @@ namespace Familytree
     public class FamilyApp
     {
         private List<Person> _people;
+
         public FamilyApp(params Person[] people)
         {
             _people = new List<Person>(people);
         }
 
-       
+
 
         public string WelcomeMessage = "Velkommen til familieappen";
         public string CommandPrompt = "Skriv vis og id for å søke... Eks: vis 1: \n";
@@ -30,7 +31,7 @@ namespace Familytree
                     return id;
                 }
             }
-            
+
             Console.WriteLine("feil Kommando: " + CommandPrompt);
             return 0;
         }
@@ -42,31 +43,31 @@ namespace Familytree
             int id = CommandIsRight(command);
             if (id != 0)
             {
-                   foreach (var person in _people)
-                   {
-                      Person personFound = findPerson(id, person);
-                      if (personFound != null)
-                      {
-                        return  ListOutPersonWithChildren(personFound, id);
-                      }
+                foreach (var person in _people)
+                {
+                    Person personFound = findPerson(id, person);
+                    if (personFound != null)
+                    {
+                        return ListOutPersonWithChildren(personFound, id);
+                    }
 
-                   }
+                }
             }
-           
+
             return $"Fant ingen ved id = {id}...";
         }
 
         private string ListOutPersonWithChildren(Person person, int id)
         {
             string personWithRightIdReturn = person.GetDescription();
-            
+
             personWithRightIdReturn += ReturnChildren(id);
 
             return personWithRightIdReturn;
         }
 
 
-       private Person findPerson(int id, Person person)
+        private Person findPerson(int id, Person person)
         {
             if (person.ID == id)
             {
@@ -78,31 +79,67 @@ namespace Familytree
             }
         }
 
-       private String ReturnChildren(int id)
-       {
-           string personWithRightIdReturn = "";
-           bool firstTime = true;
-           foreach (var child in _people)
-           {
-               if (child.Father != null)
-               {
-                   if (child.Father.ID == id)
-                   {
-                       if (firstTime)
-                       {
-                           personWithRightIdReturn += $"\n";
-                           personWithRightIdReturn += $"  Barn:\n";
-                           firstTime = false;
-                       }
-                       
+        private String ReturnChildren(int id)
+        {
+            string personWithRightIdReturn = "";
+           personWithRightIdReturn += ReturnChildrenOfMother(id);
+           personWithRightIdReturn += ReturnChildrenOfFather(id);
+            return personWithRightIdReturn;
+        }
+
+        private string ReturnChildrenOfFather(int id)
+        {
+            string personWithRightIdReturn = "";
+            bool firstTime = true;
+
+
+            foreach (var child in _people)
+            {
+                if (child.Father != null)
+                {
+                    if (child.Father.ID == id)
+                    {
+                        if (firstTime)
+                        {
+                            personWithRightIdReturn += $"\n";
+                            personWithRightIdReturn += $"  Barn:\n";
+                            firstTime = false;
+                        }
+
                         personWithRightIdReturn +=
-                           $"    {child.FirstName} {child.LastName}(Id={child.ID}) Født: {child.BirthYear}\n";
-                   }
-               }
+                            $"    {child.FirstName} {child.LastName}(Id={child.ID}) Født: {child.BirthYear}\n";
+                    }
+                }
 
-           }
+            }
 
-           return personWithRightIdReturn;
-       }
+            return personWithRightIdReturn;
+        }
+        private string ReturnChildrenOfMother(int id)
+        {
+            string personWithRightIdReturn = "";
+            bool firstTime = true;
+            foreach (var child in _people)
+            {
+                if (child.Mother != null)
+                {
+                    if (child.Mother.ID == id)
+                    {
+                        if (firstTime)
+                        {
+                            personWithRightIdReturn += $"\n";
+                            personWithRightIdReturn += $"  Barn:\n";
+                            firstTime = false;
+                        }
+
+                        personWithRightIdReturn +=
+                            $"    {child.FirstName} {child.LastName}(Id={child.ID}) Født: {child.BirthYear}\n";
+                    }
+                }
+
+            }
+
+            return personWithRightIdReturn;
+        }
     }
 }
